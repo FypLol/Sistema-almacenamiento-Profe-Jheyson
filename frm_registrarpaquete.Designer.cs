e6bbcd5f0414c803a78b261bac96174fb96c5471
Joseth <Joseth@DESKTOP-HBB5D0V>
2025-10-12 21:25:22 -0500

    Joseth v3.0

--- frm_registrarpaquete.Designer.cs
+++ frm_registrarpaquete.Designer.cs
@@ -43,10 +43,14 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_dni = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRegistrarPaquetes)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_nombreDestinatario
@@ -87,7 +91,7 @@
             // 
             // btnRegistrarP
             // 
-            this.btnRegistrarP.Location = new System.Drawing.Point(766, 115);
+            this.btnRegistrarP.Location = new System.Drawing.Point(588, 188);
             this.btnRegistrarP.Name = "btnRegistrarP";
             this.btnRegistrarP.Size = new System.Drawing.Size(243, 45);
             this.btnRegistrarP.TabIndex = 4;
@@ -97,7 +101,7 @@
             // 
             // btn_Liberar
             // 
-            this.btn_Liberar.Location = new System.Drawing.Point(766, 188);
+            this.btn_Liberar.Location = new System.Drawing.Point(837, 188);
             this.btn_Liberar.Name = "btn_Liberar";
             this.btn_Liberar.Size = new System.Drawing.Size(243, 45);
             this.btn_Liberar.TabIndex = 5;
@@ -177,12 +181,41 @@
             this.pictureBox3.TabIndex = 14;
             this.pictureBox3.TabStop = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(766, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // lbl_dni
+            // 
+            this.lbl_dni.AutoSize = true;
+            this.lbl_dni.Location = new System.Drawing.Point(688, 133);
+            this.lbl_dni.Name = "lbl_dni";
+            this.lbl_dni.Size = new System.Drawing.Size(26, 13);
+            this.lbl_dni.TabIndex = 16;
+            this.lbl_dni.Text = "DNI";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(617, 126);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 25);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
             // frm_registrarpaquete
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
             this.ClientSize = new System.Drawing.Size(1129, 582);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.lbl_dni);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -205,6 +238,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -226,5 +260,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_dni;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
