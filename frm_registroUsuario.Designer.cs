a8847f24f4a501614c726b4518847b9298be997a
Jorge Zapata <jorgezapata990213239@gmail.com>
2025-11-05 06:29:54 -0300

    ssss

--- frm_registroUsuario.Designer.cs
+++ frm_registroUsuario.Designer.cs
@@ -38,55 +38,79 @@
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
             this.lblTitulo.Location = new System.Drawing.Point(60, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(336, 38);
+            this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Registrar Nuevo Usuario";
             // 
             // lblUsuario
             // 
             this.lblUsuario.Location = new System.Drawing.Point(30, 70);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(100, 23);
+            this.lblUsuario.TabIndex = 1;
             this.lblUsuario.Text = "Usuario:";
             // 
             // lblContrasena
             // 
             this.lblContrasena.Location = new System.Drawing.Point(30, 110);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(100, 23);
+            this.lblContrasena.TabIndex = 2;
             this.lblContrasena.Text = "Contraseña:";
             // 
             // lblConfirmar
             // 
             this.lblConfirmar.Location = new System.Drawing.Point(30, 150);
+            this.lblConfirmar.Name = "lblConfirmar";
+            this.lblConfirmar.Size = new System.Drawing.Size(100, 23);
+            this.lblConfirmar.TabIndex = 3;
             this.lblConfirmar.Text = "Confirmar contraseña:";
             // 
             // txtUsuario
             // 
             this.txtUsuario.Location = new System.Drawing.Point(180, 67);
-            this.txtUsuario.Width = 150;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(150, 26);
+            this.txtUsuario.TabIndex = 4;
             // 
             // txtContrasena
             // 
             this.txtContrasena.Location = new System.Drawing.Point(180, 107);
-            this.txtContrasena.Width = 150;
+            this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
+            this.txtContrasena.Size = new System.Drawing.Size(150, 26);
+            this.txtContrasena.TabIndex = 5;
             // 
             // txtConfirmar
             // 
             this.txtConfirmar.Location = new System.Drawing.Point(180, 147);
-            this.txtConfirmar.Width = 150;
+            this.txtConfirmar.Name = "txtConfirmar";
             this.txtConfirmar.PasswordChar = '*';
+            this.txtConfirmar.Size = new System.Drawing.Size(150, 26);
+            this.txtConfirmar.TabIndex = 6;
             // 
             // btnRegistrar
             // 
             this.btnRegistrar.Location = new System.Drawing.Point(60, 200);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 7;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
             // 
             // btnCancelar
             // 
             this.btnCancelar.Location = new System.Drawing.Point(180, 200);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 8;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // frm_registroUsuario
             // 
-            this.ClientSize = new System.Drawing.Size(370, 260);
+            this.ClientSize = new System.Drawing.Size(525, 321);
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.lblUsuario);
             this.Controls.Add(this.lblContrasena);
@@ -96,9 +120,11 @@
             this.Controls.Add(this.txtConfirmar);
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.btnCancelar);
+            this.Name = "frm_registroUsuario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
     }
 }
